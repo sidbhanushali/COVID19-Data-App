f0da85d2dc3c85954ad7066278423c01f3d421d6
sid <sid@sids-MacBook-Pro.local>
2020-05-09 03:39:11 -0700

    Updates

---
+++ precache-manifest.e068546a8ac06b1c04379d2a93c369c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ca3f9766c44645bb04f1f3c938fe12e",
+    "url": "/COVID19-Data-App/index.html"
+  },
+  {
+    "revision": "47b7dac09f94a37383a0",
+    "url": "/COVID19-Data-App/static/css/main.2f48367e.chunk.css"
+  },
+  {
+    "revision": "96ef299d791bdf72cb2c",
+    "url": "/COVID19-Data-App/static/js/2.7682a2fb.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID19-Data-App/static/js/2.7682a2fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47b7dac09f94a37383a0",
+    "url": "/COVID19-Data-App/static/js/main.e7253065.chunk.js"
+  },
+  {
+    "revision": "b7848bb5ce505ce3b49f",
+    "url": "/COVID19-Data-App/static/js/runtime-main.1e59c988.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID19-Data-App/static/media/image-2.d7265326.png"
+  }
+]);
