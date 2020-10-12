b6274f66e65f1f4b6027dd0b36cb7f65e7d1db99
sid <sid@sids-MacBook-Pro.local>
2020-10-12 01:46:37 -0700

    add paper materialui background for country picker comp

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { NativeSelect, FormControl } from "@material-ui/core";
-
+import { NativeSelect, FormControl, Paper } from "@material-ui/core";
 import { fetchCountries } from "../../api";
 
 import styles from "./CountryPicker.module.css";
@@ -17,19 +16,21 @@ const Countries = ({ handleCountryChange }) => {
   }, []);
 
   return (
-    <FormControl className={styles.formControl}>
-      <NativeSelect
-        defaultValue=''
-        onChange={(e) => handleCountryChange(e.target.value)}
-      >
-        <option value=''>Global</option>
-        {countries.map((country, i) => (
-          <option key={i} value={country}>
-            {country}
-          </option>
-        ))}
-      </NativeSelect>
-    </FormControl>
+    <Paper>
+      <FormControl className={styles.formControl}>
+        <NativeSelect
+          defaultValue=''
+          onChange={(e) => handleCountryChange(e.target.value)}
+        >
+          <option value=''>Global</option>
+          {countries.map((country, i) => (
+            <option key={i} value={country}>
+              {country}
+            </option>
+          ))}
+        </NativeSelect>
+      </FormControl>
+    </Paper>
   );
 };
 
